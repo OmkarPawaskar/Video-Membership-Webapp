4b3883d98f923ca1a7fd9743bc4e86f4bcdca5ba
Omkar Pawaskar <omkarpawaskar96@hotmail.com>
2021-12-30 00:33:55 +0530

    FastAPI Form Inheritance, Login and Signup Forms

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import pathlib
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from cassandra.cqlengine.management import sync_table
@@ -31,6 +31,39 @@ def homepage(request : Request):
         "abc" : "abc"
     }
     return templates.TemplateResponse("home.html", context)
+
+@app.get('/login', response_class=HTMLResponse)
+def login_get_view(request : Request):
+    return templates.TemplateResponse("auth/login.html", {
+        "request" : request
+    })
+
+@app.post('/login', response_class=HTMLResponse)
+def login_get_view(request : Request,
+    email : str = Form(...),
+    password : str = Form(...)):
+    print(email, password)
+    return templates.TemplateResponse("auth/login.html", {
+        "request" : request
+    })
+
+
+@app.get('/signup', response_class=HTMLResponse)
+def signup_get_view(request : Request):
+    return templates.TemplateResponse("auth/login.html", {
+        "request" : request
+    })
+
+@app.post('/signup', response_class=HTMLResponse)
+def signup_get_view(request : Request,
+    email : str = Form(...),
+    password : str = Form(...),
+    password_confirm : str = Form(...)):
+    print(email, password, password_confirm)
+    return templates.TemplateResponse("auth/login.html", {
+        "request" : request
+    })
+
 
 @app.get('/users')
 def users_list_view():
