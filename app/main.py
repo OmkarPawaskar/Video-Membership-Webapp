2b477a8b710a36e5b0e81010299b8ee104903b9c
Omkar Pawaskar <omkarpawaskar96@hotmail.com>
2022-01-06 01:42:52 +0530

    Adding Shortcut to simplify template rendering

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import json
 import pathlib
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+
 from cassandra.cqlengine.management import sync_table
 from pydantic.error_wrappers import ValidationError
 from .users.models import User
@@ -10,14 +10,12 @@ from .users.schemas import (
     UserSignupSchema,
     UserLoginSchema
 )
-from . import db, utils
-
-BASE_DIR = pathlib.Path(__file__).resolve().parent #app/
-TEMPLATE_DIR = BASE_DIR / "templates"
+from . import db, utils, shortcuts
+from .shortcuts import render
 
 
 app = FastAPI()
-templates = Jinja2Templates(directory= str(TEMPLATE_DIR))
+
 
 #settings = get_settings()
 DB_SESSION = None
@@ -32,16 +30,13 @@ def on_startup():
 @app.get('/', response_class=HTMLResponse)
 def homepage(request : Request):
     context = {
-        "request" : request,
         "abc" : "abc"
     }
-    return templates.TemplateResponse("home.html", context)
+    return render(request, "home.html", context)
 
 @app.get('/login', response_class=HTMLResponse)
 def login_get_view(request : Request):
-    return templates.TemplateResponse("auth/login.html", {
-        "request" : request
-    })
+    return render(request, "auth/login.html", {})
 
 @app.post('/login', response_class=HTMLResponse)
 def login_get_view(request : Request,
@@ -52,16 +47,12 @@ def login_get_view(request : Request,
         "password" : password,
     }
     data, errors = utils.valid_schema_data_or_error(raw_data, UserLoginSchema)
-    return templates.TemplateResponse("auth/login.html", {
-        "request" : request
-    })
+    return render(request,"auth/login.html", {})
 
 
 @app.get('/signup', response_class=HTMLResponse)
 def signup_get_view(request : Request):
-    return templates.TemplateResponse("auth/signup.html", {
-        "request" : request
-    })
+    return render(request,"auth/signup.html", {})
 
 @app.post('/signup', response_class=HTMLResponse)
 def signup_get_view(request : Request,
@@ -75,8 +66,7 @@ def signup_get_view(request : Request,
         "password_confirm" : password_confirm
     }
     data, errors = utils.valid_schema_data_or_error(raw_data, UserSignupSchema)
-    return templates.TemplateResponse("auth/signup.html", {
-        "request" : request,
+    return render(request,"auth/signup.html", {
         "data" : data,
         "errors" : errors,
     })
