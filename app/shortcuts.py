51ea7bb36dea02d0becdd93e780cb107af527067
Omkar Pawaskar <omkarpawaskar96@hotmail.com>
2022-01-07 00:30:15 +0530

    Using Cookies and httponly in FastApi

--- app/shortcuts.py
+++ app/shortcuts.py
@@ -1,3 +1,4 @@
+from pydantic.networks import validate_email
 from app import config
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -6,12 +7,23 @@ from fastapi.templating import Jinja2Templates
 settings = config.get_settings()
 templates = Jinja2Templates(directory= str(settings.templates_dir))
 
-def render(request, template_name, context = {} , status_code: int = 200):
+def render(request, template_name, context = {} , status_code: int = 200, cookies:dict = {}):
     ctx = context.copy()
     ctx.update({"request" : request})
     t = templates.get_template(template_name)
     html_str =  t.render(ctx)
     #set http only cookies
     responses = HTMLResponse(html_str, status_code = status_code)
+    #print(responses.cookies)
+    responses.set_cookie(key="darkmode", value=1)
+
+    if len(cookies.keys) > 0:
+        #set http only cookies
+        for k,v in cookies.items():
+            responses.set_cookie(key=k ,value=v ,httponly=True) #httponly arg ensures user is not able to edit via js console.
+    
+    #delete cookie
+    #for key in cookies.keys():
+    #    responses.delete_cookie(key)
     return responses
     #return templates.TemplateResponse(template_name, ctx)
