e2ad8dc5a8b65aff352c337fb32384696ce122fd
Omkar Pawaskar <omkarpawaskar96@hotmail.com>
2022-01-11 00:16:19 +0530

    Notebook to verify Add feature

--- app/videos/models.py
+++ app/videos/models.py
@@ -13,7 +13,7 @@ class Video(Model):
     __keyspace__ = settings.keyspace
     host_id = columns.Text(primary_key = True) #Youtube,DailyMotion
     db_id = columns.UUID(primary_key=True, default=uuid.uuid1)
-    host_service = columns.Text(default=True)
+    host_service = columns.Text(default="youtube")
     title = columns.Text()
     url = columns.Text() #secure
     user_id = columns.UUID()
@@ -33,12 +33,12 @@ class Video(Model):
         host_id = extract_video_id(url)
         if host_id is None:
             raise Exception("Invalid Youtube video URL")
-        user_id = User.check_exist(user_id)
-        if user_id is None:
+        user_exist = User.check_exist(user_id)
+        if user_exist is None:
             raise Exception("Invalid user_id")
         # user_obj = User.by_user_id(user_id)
         # user_obj.display_name
-        q = Video.objects.filter(host_id=host_id, user_id=user_id)
+        q = Video.objects.allow_filtering().filter(host_id=host_id, user_id=user_id)
         if q.count() != 0:
             raise Exception("Video Already Added!")
         return Video.create(host_id=host_id,user_id=user_id,url=url)
