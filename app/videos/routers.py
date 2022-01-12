6e1b238eb2c4556672908d8833547ab74b7600d3
Omkar Pawaskar <omkarpawaskar96@hotmail.com>
2022-01-12 16:40:44 +0530

    Video Create view

--- app/videos/routers.py
+++ app/videos/routers.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from app.shortcuts import render
+from app.users.decorators import login_required
 
 router = APIRouter(
     prefix="/videos"
 )
+
+@router.get('/create', response_class=HTMLResponse)
+@login_required
+def video_create_view(request: Request):
+    return render(request,"videos/create.html", {})
 
 @router.get('/', response_class=HTMLResponse)
 def video_list_view(request: Request):
