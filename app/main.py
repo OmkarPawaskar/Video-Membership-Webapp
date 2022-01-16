e6fb825e6389ccbcfac7b5fa90c8766b5531b414
Omkar Pawaskar <omkarpawaskar96@hotmail.com>
2022-01-16 20:34:35 +0530

    Watch Events Model

--- app/main.py
+++ app/main.py
@@ -14,6 +14,9 @@ from .users.schemas import (
 )
 from .users.decorators import login_required
 from .videos.routers import router as video_router
+from .videos.models import Video
+
+from .watch_events.models import WatchEvent
 from . import db, utils, shortcuts
 from .shortcuts import redirect, render
 
@@ -33,6 +36,8 @@ def on_startup():
     global DB_SESSION
     DB_SESSION = db.get_session()
     sync_table(User)
+    sync_table(Video)
+    sync_table(WatchEvent)
 
 @app.get('/', response_class=HTMLResponse)
 def homepage(request : Request):
@@ -107,5 +112,13 @@ def users_list_view():
 @app.post('/watch-event')
 def watch_event_view(request : Request, data : dict):
     print(data)
-    print(request.user.is_authenticated)
+    if request.user.is_authenticated:
+        WatchEvent.objects.create(
+            host_id = data.get('videoId'),
+            user_id = request.user.username,
+            start_time = 0,
+            end_time = data.get('currentTime'),
+            duration = 500,
+            complete = False
+        )
     return {"working" : True}
